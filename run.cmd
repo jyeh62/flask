3375ea52735db68f8a5137d35aca33fca740fa3c
JY Choi <jyeh62@gmail.com>
2021-06-04 13:14:58 +0900

    add window script

---
+++ run.cmd
@@ -0,0 +1,7 @@
+@echo off
+cd c:/Users/jychoi/PycharmProjects/flask
+# --------------------------------- [edit] ---------------------------------- #
+set FLASK_APP=pybo
+set FLASK_ENV=development
+# --------------------------------------------------------------------------- #
+c:/Users/jychoi/PycharmProjects/flask/venv/scripts/activate
